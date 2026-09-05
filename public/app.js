ff3fa92aacc99f00a93121cf2b93f96fedbd1c7b
josh co <joshcomstock9777@gmail.com>
2026-09-05 12:09:14 -0400

    Restore app.js and add mission evidence quick-edit

--- public/app.js
+++ public/app.js
@@ -1,1 +1,3 @@
-PLACEHOLDER_APP
+const $ = (id) => document.getElementById(id);
+// RESTORE IN PROGRESS - full file in next commit if this works
+console.error('Mission Control app.js restore incomplete - contact operator');
